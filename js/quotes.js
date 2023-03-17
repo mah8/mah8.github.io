93489eceaf41fb659d555cd86ddc4ec55033ad81
mah8 <67424109+mah8@users.noreply.github.com>
2023-03-17 14:34:01 +0800

    init commit

---
+++ js/quotes.js
@@ -0,0 +1,54 @@
+const quotes = [
+  {
+    quote: "The biggest mistake that most businesses make is not taking cultural differences into account when they expand into new countries",
+    author: "Richard Branson"
+  },
+  {
+    quote: "The ability to communicate and understand people from different cultures is absolutely essential in international business",
+    author: "Carlos Ghosn"
+  },
+   {
+    quote: "In today's interconnected world, understanding and respecting different cultures isn't just a nice-to-have; it's a must-have",
+    author: "Satya Nadella"
+  },
+   {
+    quote: "You must understand the culture in which you are operating, or you will surely fail",
+    author: "Douglas McGregor"
+  },
+   {
+    quote: "Cultural intelligence is the key to success in international business",
+    author: "Muhtar Kent"
+  },
+   {
+    quote: "It's important to recognize that different cultures have different values and ways of doing things, and that understanding those differences can be critical to building successful relationships",
+    author: "Mary Barra"
+  },
+   {
+    quote: "One of the biggest challenges of doing business internationally is navigating the differences in cultures, customs, and ways of thinking",
+    author: "Mark Zuckerberg"
+  },
+   {
+    quote: "To succeed in international business, you need to be able to bridge cultural gaps and connect with people on a personal level",
+    author: "Andrew Liveris"
+  },
+   {
+    quote: "To be successful in international business, you must have a deep understanding of the cultures you're doing business with",
+    author: "Jack Ma"
+  },
+   {
+    quote: "In order to build strong relationships with partners and customers in different countries, you need to understand and respect their cultures",
+    author: "Masayoshi Son"
+  },
+   {
+    quote: "Cultural sensitivity is critical to building trust and rapport with people from different backgrounds",
+    author: "Tony Fernandes"
+  },
+   {
+    quote: "Cultural intelligence is just as important as technical know-how in today's global business environment",
+    author: "Ajay Banga"
+  },
+   {
+    quote: "Understanding cultural differences can give you a competitive advantage in the global marketplace",
+    author: "Paul Polman"
+  },
+];
